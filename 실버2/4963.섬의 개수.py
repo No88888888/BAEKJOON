b5daf711090c02ff11b604bbc307abef37b08a9f
No88888888 <isagkim@gmail.com>
2022-10-01 00:36:56 +0900

    문제추가

--- 실버2/4963.섬의 개수.py
+++ 실버2/4963.섬의 개수.py
@@ -25,7 +25,6 @@ def find_set(x,y):
 
 def union(x,y,a,b):
     rep[a][b] = find_set(x,y)
-    # rep[find_set(a,b)] = find_set(x,y)
 
 w, h = 1, 1
 while w != 0 and h != 0:
@@ -35,29 +34,70 @@ while w != 0 and h != 0:
     rep = [[0]*w for _ in range(h)]
     for i in range(h):
         for j in range(w):
-            rep[i][j] = (i,j)
-    mmap = [list(map(int, input().split())) for _ in range(h)]
+            rep[i][j] = (i,j)           # 각 인덱스 별 고유 대표원소 기본값 저장(자기자신, 2차원좌표 튜플형태)
+    mmap = [list(map(int, input().split())) for _ in range(h)]  # 지도
+    
     visited = [[0]*(w) for _ in range(h)]
-    i, j = 0, 0
-    # while 0<= i <= h-1 and 0<= j <= w-1:
     for i in range(h):
         for j in range(w):
-            if mmap[i][j] == 1:
-                for di, dj in [[-1,0],[1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]:
-                    ni, nj = i + di, i+dj
-                    if 0<=ni<h and 0<=nj<w and visited[ni][nj] == 0 and mmap[ni][nj]:
-                        visited[ni][nj] == 1
-                        union(i,j,ni,nj)
-                        ni, nj = i, j
-    print(rep)
+            if mmap[i][j] == 1:     # 1이라면
+                stack = [[i,j]]     
+                while stack:        # 깊이 우선 탐색
+                    x, y = stack.pop()
+                    for di, dj in [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]:
+                        ni, nj = x + di, y+dj
+                        if 0<=ni<h and 0<=nj<w and visited[ni][nj] == 0 and mmap[ni][nj]:
+                            visited[ni][nj] = 1
+                            union(i,j,ni,nj)        # 붙은 땅끼리 union(대표원소 맞춰주기)
+                            stack.append([ni,nj])
     res = set()
     for i in rep:
         for j in i:
-            res.add(tuple(j))
+            res.add(tuple(j))   # rep에서 대표원소들만 set으로 정렬하여 res에 저장
     cnt0 = 0
     for i in mmap:
-        cnt0 += i.count(0)
+        cnt0 += i.count(0)      # 지도에서 바다의 개수를 셈
     
-    # print(cnt0)
-    print(len(res)-cnt0)
+    print(len(res)-cnt0)        # res - 바다의 개수 = 섬의 개수
 
+
+
+# def find_set(x,y):
+#     while rep[x][y] != (x, y):
+#         (x,y) = rep[x][y]
+#     return (x,y)
+
+# def union(x,y,a,b):
+#     rep[a][b] = find_set(x,y)
+#     # rep[find_set(a,b)] = find_set(x,y)
+
+# w, h = 1, 1
+# while w != 0 and h != 0:
+#     w, h = map(int, input().split())
+#     if w == 0 and h == 0:
+#         break
+#     rep = [[0]*w for _ in range(h)]
+#     for i in range(h):
+#         for j in range(w):
+#             rep[i][j] = (i,j)
+#     mmap = [list(map(int, input().split())) for _ in range(h)]
+#     visited = [[0]*(w) for _ in range(h)]
+#     # while 0<= i <= h-1 and 0<= j <= w-1:
+#     for i in range(h):
+#         for j in range(w):
+#             if mmap[i][j] == 1:
+#                 for di, dj in [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]:
+#                     ni, nj = i + di, j+dj
+#                     if 0<=ni<h and 0<=nj<w and visited[ni][nj] == 0 and mmap[ni][nj]:
+#                         visited[ni][nj] = 1
+#                         union(i,j,ni,nj)
+#                         i, j = ni, nj 
+#     res = set()
+#     for i in rep:
+#         for j in i:
+#             res.add(tuple(j))
+#     cnt0 = 0
+#     for i in mmap:
+#         cnt0 += i.count(0)
+    
+#     print(len(res)-cnt0)
