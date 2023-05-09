2248bb8766cfd3c163a22274e2333f7b0e55c00f
No88888888 <isagkim@gmail.com>
2023-05-10 00:25:15 +0900

    0510

---
+++ baekjoon_java/src/silver1/findroad_11403.java
@@ -0,0 +1,24 @@
+package silver1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class findroad_11403 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            tree
+        }
+
+    }
+}
